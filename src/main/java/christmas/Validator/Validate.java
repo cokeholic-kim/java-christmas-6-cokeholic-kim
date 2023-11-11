c9632fc0a0c4cb018a425ffb3c97100452de6f6b
dong_hy <kimadonghyon99@gmail.com>
2023-11-11 23:32:56 +0900

    1. 메뉴의 갯수가 1개이상 20개이하인지 검증하는 메서드 추가

--- src/main/java/christmas/Validator/Validate.java
+++ src/main/java/christmas/Validator/Validate.java
@@ -4,39 +4,61 @@ import static christmas.ErrorMessage.INVALID_ORDER_MESSAGE;
 
 import christmas.Menu;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Validate {
-    public static void validateOrder(String input){
+
+    private static final int MINIMUM_QUANTITY = 1;
+    private static final int MAXIMAL_QUANTITY = 20;
+    public static void validateOrder(String input) {
+        //검증순서는 가장 실수가 많을것같다고 생각되는부분부터 검증시도.
         checkMenuAvailability(input);
-        //        메뉴의 개수가 1미만인 경우, "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."라는 에러 메시지를 보여 준다.
+        checkMenuCount(input);
         //        메뉴 형식이 예시와 다른 경우, "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."라는 에러 메시지를 보여 준다.
         //        메뉴가 중복된 경우(e.g. 시저샐러드-1,시저샐러드-1), "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."라는 에러 메시지를 보여 준다.
-        //        메뉴가 20개를 초과하는 경우
         //        음료만 주문하는 경우
     }
 
-    private static void checkMenuAvailability(String orders){
-//        메뉴에 없는 메뉴를 입력하는 경우, "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."라는 에러 메시지를 보여 준다.
-        Map<String,Integer> orderMenu = orderMapper(orders);
-        orderMenu.keySet().stream().forEach(menuName ->{
+    private static void checkMenuCount(String input) {
+        //메뉴의 개수가 1미만인 경우, "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."라는 에러 메시지를 보여 준다.
+        //메뉴가 20개를 초과하는 경우
+        int count = countMenuQuantity(orderMapper(input));
+        if(count > MAXIMAL_QUANTITY || count < MINIMUM_QUANTITY){
+            throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
+        }
+    }
 
-            if(Menu.returnMenu(menuName) == Menu.NONE){
+
+
+    private static void checkMenuAvailability(String orders) {
+        //메뉴에 없는 메뉴를 입력하는 경우, "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."라는 에러 메시지를 보여 준다.
+        Map<String, Integer> orderMenu = orderMapper(orders);
+        orderMenu.keySet().stream().forEach(menuName -> {
+
+            if (Menu.returnMenu(menuName) == Menu.NONE) {
                 throw new IllegalArgumentException(INVALID_ORDER_MESSAGE);
             }
         });
     }
 
     //TODO:이 메서드의 적합한위치 고려하여 이후에 이동
-    public static Map<String,Integer> orderMapper(String orders){
-        Map<String,Integer> menu = new LinkedHashMap<>();
-        Arrays.stream(orders.split(",",-1)).forEach(order -> {
+    //맵에 Menu와 갯수를 매핑하는형태로 변경 고려. - > 입력에대한 검증을한후에 메뉴를 맵에 매핑하는게 좋을것같음,
+    //이 메서드는 검증을 하기유용한형태로 변경해준다는 점에 집중.
+    //주문을 종이에받아적고 검증한후에 pos에 입력한다는 개념으로 접근.
+    public static Map<String, Integer> orderMapper(String orders) {
+        Map<String, Integer> menu = new LinkedHashMap<>();
+        Arrays.stream(orders.split(",", -1)).forEach(order -> {
             String[] menuInfo = order.split("-");
-            menu.put(menuInfo[0],Integer.parseInt(menuInfo[1]));
+            menu.put(menuInfo[0], Integer.parseInt(menuInfo[1]));
         });
         return menu;
+    }
+
+    private static int countMenuQuantity(Map<String,Integer> mapppedOrder){
+        return mapppedOrder.values().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
 }
--- src/test/java/christmas/Validator/ValidateTest.java
+++ src/test/java/christmas/Validator/ValidateTest.java
@@ -5,13 +5,9 @@ import static christmas.ErrorMessage.INVALID_ORDER_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Stream;
-import org.assertj.core.api.Assert;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -36,17 +32,32 @@ class ValidateTest {
         );
     }
 
-    @Test
     @DisplayName("메뉴에 없는 메뉴를 입력하는 경우")
-//    @ValueSource(strings  = {
-//            "티본스테이크-1,바비큐립-1,초코케이크-2,다이어트콜라-1"
-//    })
-    void validateOrderTest() {
-        String input = "티본스테이크-1,바비큐립-1,초코케이크-2,다이어트콜라-1";
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1",
+            "다이어트콜라-1,바베큐립-1,초코케이크-2,제로콜라-1",
+            "해잔물파스타-2,레드와인-1,초코케이크-1"})
+    void validateOrderTest(String input) {
         assertThatThrownBy(() -> {
             Validate.validateOrder(input);
         }).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage(INVALID_ORDER_MESSAGE);
-//                .hasMessageStartingWith(ERROR_PREFIX);
+                .hasMessage(INVALID_ORDER_MESSAGE)
+                .hasMessageStartingWith(ERROR_PREFIX);
+    }
+    //TODO : 테스트에 들어가는 파라미터만다르고 동작은 일치하므로 이후에 파라미터와 기대값 으로 나누어서 하나로 합치기
+    @DisplayName("메뉴의 갯수가 1개이상 20개이하가 아닌 경우")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "티본스테이크-0,바베큐립-0,시저샐러드-0,제로콜라-0",
+            "티본스테이크-19,바베큐립-2",
+            "바베큐립-19,시저샐러드-1,제로콜라-1",
+    })
+    void validateOrderCountTest(String input){
+        assertThatThrownBy(() -> {
+            Validate.validateOrder(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ORDER_MESSAGE)
+                .hasMessageStartingWith(ERROR_PREFIX);
     }
 }
