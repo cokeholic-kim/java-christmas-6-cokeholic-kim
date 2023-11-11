54f7c00ab7e49cda3fcf65b6941596d02131f9f8
dong_hy <kimadonghyon99@gmail.com>
2023-11-11 22:54:42 +0900

    feat : 에러출력에 사용할 문자열상수 분리

---
+++ src/main/java/christmas/ErrorMessage.java
@@ -0,0 +1,6 @@
+package christmas;
+
+public class ErrorMessage {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+    public static final String INVALID_ORDER_MESSAGE = ERROR_PREFIX + "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+}
