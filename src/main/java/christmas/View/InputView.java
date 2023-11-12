71bac7841d2d98b79ab2654f67529b05bd546a00
dong_hy <kimadonghyon99@gmail.com>
2023-11-12 20:29:57 +0900

    feat : InputView기능생성

---
+++ src/main/java/christmas/View/InputView.java
@@ -0,0 +1,13 @@
+package christmas.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static String readOrder(){
+        return Console.readLine();
+    }
+
+    public static String readDay(){
+        return Console.readLine();
+    }
+}
