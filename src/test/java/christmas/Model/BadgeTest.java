7898f6210b5bb3c6ee69c47196da2ae342a10bfa
dong_hy <kimadonghyon99@gmail.com>
2023-11-13 20:07:39 +0900

    feat : 뱃지Enum상수 추가 1. 할인금액을 입력받아 뱃지를 리턴해주는 메서드생성

---
+++ src/test/java/christmas/Model/BadgeTest.java
@@ -0,0 +1,19 @@
+package christmas.Model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BadgeTest {
+
+    @Test
+    @DisplayName("뱃지의 리턴값 테스트")
+    void returnBadge() {
+        int price = 31246;
+        assertThat(
+                Badge.returnBadge(price)
+        ).isEqualTo(Badge.SANTA);
+    }
+}
