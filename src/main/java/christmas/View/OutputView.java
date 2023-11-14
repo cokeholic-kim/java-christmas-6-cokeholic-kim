b3aee61305cc348c76369d99aad41fbe24c9d9ce
dong_hy <kimadonghyon99@gmail.com>
2023-11-14 16:35:58 +0900

    feat : OutputView 출력기능 추가 1. 각 값들을 받아서 출력시켜줄 메서드 추가. 2. docs: 추가된 클래스 수정

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -1,5 +1,7 @@
 package christmas.View;
 
+import christmas.Model.Badge;
+
 public class OutputView {
     public static void printWelcome(){
         System.out.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
@@ -14,6 +16,49 @@ public class OutputView {
     }
 
     public static void printEventPreView(int date){
-        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!",date);
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n%n",date);
+    }
+
+    public static void printOrderMenu(String menu){
+        System.out.println("<주문 메뉴>");
+        System.out.println(menu);
+        System.out.println();
+    }
+
+    public static void printTotalPrice(int price){
+        System.out.println("<할인 전 총주문 금액>");
+        System.out.printf("%,d원",price);
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void printGiving(String giving){
+        System.out.println("<증정 메뉴>");
+        System.out.println(giving);
+        System.out.println();
+    }
+
+    public static void printBenefitList(String benefit){
+        System.out.println("<혜택 내역>");
+        System.out.println(benefit);
+        System.out.println();
+    }
+
+    public static void printBenefitPrice(int price){
+        System.out.println("<총혜택 금액>");
+        System.out.printf("-%,d원",price);
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void printExpectedPrice(int expected){
+        System.out.println("<할인 후 예상 결제 금액>");
+        System.out.println(expected + "원");
+        System.out.println();
+    }
+
+    public static  void printBadge(Badge badge){
+        System.out.println("<12월 이벤트 배지>");
+        System.out.println(badge.getBadgeName());
     }
 }
